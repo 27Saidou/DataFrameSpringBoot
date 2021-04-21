9ab68e440c1bc0e4db4b843394369e3389115087
Saidou Diallo <sinthio2@gmail.com>
2021-04-21 16:11:16 +0000

    generation du documentation swagger.json avec springboot

--- src/main/java/com/javaexpress/org/config/SwaggerConfig.java
+++ src/main/java/com/javaexpress/org/config/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package com.javaexpress.org.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,7 +22,10 @@ public class SwaggerConfig {
 	      .select()
 	      .apis(RequestHandlerSelectors.basePackage("com.javaexpress.org"))
 	      .paths(PathSelectors.ant("/api/v1/**"))
-	      .build();
+	      .build().apiInfo(apiInfo());
 	     
+	}
+	private ApiInfo apiInfo() {
+		return new ApiInfo("Employee Project", "Employee Information for CRUD Operations", "4.0.0","www.example.com",new Contact("Saidou diallo", "www.example.com", "myeaddress@company.com"),"Apache2", "www.example.com",Collections.EMPTY_LIST);
 	}
 }
